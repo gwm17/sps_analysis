f5154c9013e338d27359c0e87fdcbd7ec6284266
Gordon McCann <gmccann@fsu.edu>
2019-04-30 16:23:59 -0400

    edited storage structure, updated Makefile

--- Makefile
+++ Makefile
@@ -1,18 +1,26 @@
 CC=g++
-CFLAGS=-c -Wall `root-config --cflags --glibs`
-LDFLAGS=`root-config --cflags --glibs`
-OBJECTS=analysis.o fit.o background.o main.o
-EXECUTABLE=analysis
-
-all: $(EXECUTABLE)
-
-$(EXECUTABLE): $(OBJECTS)
-	$(CC) $(LDFLAGS) $(OBJECTS) -o $@ -lSpectrum
-
-%.o: %.cpp
-	$(CC) -o $@ $(CFLAGS) $^ 
+ROOTFLAGS = `root-config --cflags`
+ROOTLIBS = `root-config --glibs`
+CFLAGS=-g -Wall $(ROOTFLAGS)
+INCLDIR=./include
+SRCDIR=./src
+OBJDIR=./objs
+LDFLAGS= $(ROOTLIBS)
+CPPFLAGS= -I$(INCLDIR)
+LDLIBS= -lSpectrum
+SRC=$(wildcard $(SRCDIR)/*.cpp)
+OBJS=$(SRC:$(SRCDIR)/%.cpp=$(OBJDIR)/%.o)
+EXE=./analysis
 
 .PHONY: clean
 
+all: $(EXE)
+
+$(EXE): $(OBJS)
+	$(CC) $(LDFLAGS) $^ -o $@ $(LDLIBS)
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.cpp
+	$(CC) $(CPPFLAGS) $(CFLAGS) -c $< -o $@
+
 clean:
-	rm ./*.o ./analysis
+	$(RM) $(OBJS) $(EXE)
---
+++ include/analysis.h
@@ -0,0 +1,141 @@
+/*
+analysis.h
+ROOT analysis standalone for the splitpole spectrograph
+3 steps of sorting, takes a raw data file and makes a file of histograms and cuts 
+input arguments: data file name and storage file name
+
+Dec. 2018
+Gordon M.
+
+Revised March 2019 to run without reopening and reclosing files as shown by KGH -- Gordon M.
+*/
+
+#ifndef ANALYSIS_H
+#define ANALYSIS_H
+
+#include "TROOT.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TObjArray.h"
+#include "TCutG.h"
+#include "TTree.h"
+#include "TFile.h"
+
+using namespace std;
+
+class analysis
+{
+
+  public:
+    analysis();
+    void run(char* dataName, char* storageName);
+    ~analysis();
+   
+  private:
+    /*functions*/
+    void sort_raw();
+    void sort_tclean();
+    void sort_full();
+    int notEmpty(Int_t value);
+    int Tsum1Check(Float_t value);
+    int Tsum2Check(Float_t value);
+    int SiTimeCheck(Int_t value);
+
+    /*Tree for storing final paramters*/    
+    TTree *sortTree; 
+
+    /*storage vectors, so raw file only needs opened once*/
+    vector<Int_t> anode1_v,
+    anode2_v,
+    scint1_v,
+    scint2_v;
+    
+    vector<Float_t> tdiff1_v,
+    tdiff2_v,
+    tsum1_v,
+    tsum2_v,
+    scint1_time_v,
+    anode1_time_v,
+    anode2_time_v;
+
+    vector<vector<Int_t>> mtdc_v;
+
+    /*raw branch variables*/
+    Int_t anode1_d,
+    anode2_d,
+    scint1_d,
+    scint2_d;
+
+    Float_t tsum1_d,
+    tsum2_d,
+    tdiff2_d,
+    tdiff1_d,
+    scint1_time_d,
+    anode1_time_d,
+    anode2_time_d;
+
+    vector<Int_t> *mtdc_d;
+    
+    /*new tree variables*/
+    Float_t tdiff1_n,
+    tdiff2_n,
+    tsum1_n,
+    tsum2_n,
+    tcheck2_n,
+    tcheck1_n,
+    theta_n,
+    phi_n,
+    y1_n,
+    y2_n;
+    
+    Int_t anode1_n,
+    anode2_n,
+    cutFlag_n,
+    scint1_n;
+
+    /*keep track of number of entries for loops*/
+    int nentries;
+ 
+    /*histograms, cuts, etc.*/
+    TH1F *fp1_tsum,
+    *fp1_tdiff,
+    *fp2_tsum,
+    *fp2_tdiff,
+    *si_time,
+    *fp1_tdiff_ts1a1gate,
+    *fp1_tdiff_all,
+    *fp1_tdiff_all_closed,
+    *xavg,
+    *xdiff,
+    *fp1_y,
+    *fp2_y,
+    *yavg,
+    *fp1_tdiff_all_sitime,
+    *fp1_tdiff_all_sitime_closed,
+    *phi_hist;
+
+    TH2F *scint1_anode1,
+    *fp1_anode1,
+    *fp2_anode2,
+    *x1_x2,
+    *x1_theta,
+    *fp1_anode_ts1a1gate,
+    *fp1_tdiffsum;
+
+    TObjArray *histoArray;
+
+    TCutG *s1a1_cut;
+    TCutG *x1x2_cut;
+    TCutG *fp1anode1_cut;
+    TCutG *sicoinc_cut;
+    TCutG *theta_cut;
+    Int_t minSi;
+    Int_t maxSi;
+    Int_t max1;
+    Int_t min1;
+    Int_t max2;
+    Int_t min2;
+
+} ;
+
+#endif      
